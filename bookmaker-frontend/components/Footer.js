ee64d03c60533750e9308eb029fba2af1ae8e2ae
dzikoysk <dzikoysk@dzikoysk.net>
2023-04-24 18:08:19 +0200

    Update Footer.js

--- bookmaker-frontend/components/Footer.js
+++ bookmaker-frontend/components/Footer.js
@@ -3,7 +3,7 @@ import { Container, Flex, Text } from "@chakra-ui/react"
 export default function Footer() {
   return (
     <Flex>
-      <Container>
+      <Container maxW={'container.xl'}>
         <Text textAlign={'center'} paddingY='4' fontSize={'sm'}>
           Wizualny prototyp interfejsu | Praca inżynierska 117E-ISP-IZ/305446/1200369 | Copyright © 2023 Szymon Jazgara
         </Text>
